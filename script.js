6c6d94f77ac30e025c6ca0602c9216b0088d160f
softwareNinja-888 <mlandvodlamini03@gmail.com>
2024-08-02 08:44:30 +0200

    Add all features and flow of game

--- script.js
+++ script.js
@@ -3,25 +3,70 @@
 
 
 function Game(){
-    let sum = 100;
+    let size = 100;
+
+    let value = true;
 
     const angel = document.querySelector(".angel")
     const angelScore = document.querySelector(".Anscore")
     const devilScore = document.querySelector(".Descore")
+    const time = document.querySelector(".time")
+    let timeVar = Number(time.textContent)
     
-    const timer = ()=>{}
+    const timer = (on)=>{
+        if (on === true){
 
-    const counter = ()=>{
+            value = false
 
+            let sum = 10;
+
+            let int = setInterval(()=>{
+                sum--
+                time.textContent = sum
+                if (sum === 0 ){
+                    clearInterval(int)
+                    displayWinner(angelScore,devilScore)
+                    reset() 
+                }
+            },1000)
+
+           
+        }
+        else if (on === false) {
+            return 0
+        }
+    }
+
+    const counter = () => {
         angel.addEventListener("click",()=>{
-            angelScore.innerHTML = Number(angelScore.innerHTML) + 200
-            sum++
-            angel.style.width  = sum + "px";
+            timer(value)
+            angelScore.textContent = Number(angelScore.textContent) + 200
+            size++
+            angel.style.width  = size + "px";
         })
     }
     counter();
 
-    const displayWinner = ()=>{}
+    const displayWinner = (a,d)=>{
+        let Anum = Number(a.textContent)
+        let Dnum = Number(d.textContent)
+
+        if (Anum > Dnum) {
+            alert("Angel Wins!!!")
+        }else if (Dnum > Anum){
+            alert("You failed")
+        } else {
+            alert("Draw")
+        }
+    }
+
+    const reset = ()=>{
+        angelScore.textContent = 0
+        value = true
+        time.textContent = 10
+        size = 100
+        angel.style.width  = 100 + "px";
+    }
 }
 
 Game();
